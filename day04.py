83c760e63bd3047cd8e93f5466d4ad9e1441ce21
John Gillen <john.gillen@trojans.dsu.edu>
2022-12-04 00:19:54 -0600

    refactor day 4

--- day04.py
+++ day04.py
@@ -9,13 +9,12 @@ allpairs = 0
 for line in lines:
     a, b, c, d = [int(n) for n in re.findall(r'\d+', line)]
 
-    elf1 = set(range(int(a), int(b) + 1))
-    elf2 = set(range(int(c), int(d) + 1))
+    elf1 = set(range(a, b + 1))
+    elf2 = set(range(c, d + 1))
 
-    if elf1.issubset(elf2) or \
-       elf2.issubset(elf1):
+    if elf1 | elf2 in (elf1, elf2):
         pairs += 1
-    if elf1.intersection(elf2):
+    if elf1 & elf2:
         allpairs += 1
 
 part1 = pairs
