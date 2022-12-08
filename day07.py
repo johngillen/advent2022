59431f7aff24926389e7bb6eba026240fdc29552
John Gillen <john.gillen@trojans.dsu.edu>
2022-12-07 19:33:06 -0600

    refactor + day 08 prep

--- day07.py
+++ day07.py
@@ -1,28 +1,25 @@
 f = open('input/day07.txt')
 lines = [line.rstrip() for line in f.readlines()]
 
-part1 = 0
-part2 = 0
-
-path = []
+pwd = []
 directories = {'/': 0}
 
 for line in lines:
     if '$ cd' in line:
-        if line.split()[-1] == '..':
-            path.pop()
+        if '..' not in line:
+            pwd.append(line.split()[-1])
         else:
-            path.append(line.split()[-1])
+            pwd.pop()
     elif line.split()[0].isdigit():
         size = int(line.split()[0])
         pathname = '/'
-        for dir in path:
+        for dir in pwd:
             pathname += (dir + '/') if dir != '/' else ''
             if pathname not in directories:
                 directories[pathname] = 0
             directories[pathname] += size
 
-part1 += sum([v for v in directories.values() if v <= 100000])
+part1 = sum([v for v in directories.values() if v <= 100000])
 part2 = min([v for v in directories.values() if v >= \
     (30000000 - (70000000 - directories['/']))])
 
---
+++ day08.py
@@ -0,0 +1,11 @@
+f = open('input/day08.txt')
+lines = [line.rstrip() for line in f.readlines()]
+
+part1 = 0
+part2 = 0
+
+for line in lines:
+    print(line)
+
+print(f'part 1: {part1}')
+print(f'part 2: {part2}')
